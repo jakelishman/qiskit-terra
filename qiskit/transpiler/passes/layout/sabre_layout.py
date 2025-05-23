1d96e490304bb9d59b8a8dcca5b904d736025a47
Jake Lishman <jake.lishman@ibm.com>
2025-05-23 16:07:26 +0100

    Improve Sabre heuristic defaults
    This commit removes the scaling of the basic heuristic of Sabre
    inversely proportional to the front-layer population in all of
    Qiskit's defaults.
    
    Previously, the "basic" heuristic of Sabre had a scaling factor applied
    to it such that the _total_ contribution of the front layer to the
    full score of a swap remained approximately the same magnitude (scaled
    by the sums of the distances), no matter how many gates were in the
    layer.  However, since the implementation of relative scoring[^1], we
    realise that only the score delta caused by a swap matters.  A swap can
    affect at most two gates in the front layer, so scaling down the impact
    of this based on the population of the front layer only has the effect
    of allowing other heuristics to entirely dominate the "basic" component,
    when really the basic heuristic is the most critical for forwards
    progress in the algorithm.
    
    In the worst-case scenario of a front-layer populated with more than 40
    gates, the extended set with a fixed size of 20 gates, a relative weight
    of 0.5 and similar population-based scaling would entirely override the
    front-layer heuristic, which would only be used as a tie-break.  For
    such circuits, this caused the release-valve mechanism to trigger on
    almost every gate in the worst cases, completely destroying the
    performance and quality of the lookahead methods.  This was observed in
    currently unrealistic circuits like a 1081-width, 1081-depth volume
    circuit, and 400+ qubit QFTs, but is likely to have had some effect at
    smaller circuits.
    
    [^1]: 02a193919c: Calculate relative, not absolute, scores in SabreSwap

--- qiskit/transpiler/passes/layout/sabre_layout.py
+++ qiskit/transpiler/passes/layout/sabre_layout.py
@@ -261,7 +261,7 @@ class SabreLayout(TransformationPass):
         ]
         heuristic = (
             Heuristic(attempt_limit=10 * self.target.num_qubits)
-            .with_basic(1.0, SetScaling.Size)
+            .with_basic(1.0, SetScaling.Constant)
             .with_lookahead(0.5, 20, SetScaling.Size)
             .with_decay(0.001, 5)
         )
